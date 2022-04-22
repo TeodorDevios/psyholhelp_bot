6f768da70d631e8b7c469d416bd117ea32e2e7a6
TeodorDevios <stepantimoshilov@yandex.ru>
2022-04-22 22:54:47 +0300

    И еще немного кода

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 from aiogram import executor
 from config import dp
+from client import init_client
 
 
 async def on_startup(_):
     print('Я работаю, хозяинка')
 
 
+init_client(dp)
 executor.start_polling(dp, skip_updates=True)
