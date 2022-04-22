0665d7f7c53e823a1e8aed5029f27e5a989a0ae1
TeodorDevios <stepantimoshilov@yandex.ru>
2022-04-22 22:43:49 +0300

    Добавлено немного кода в bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
-import aiogram
+from aiogram import executor
+from config import dp
+
+
+async def on_startup(_):
+    print('Я работаю, хозяинка')
+
+
+executor.start_polling(dp, skip_updates=True)
